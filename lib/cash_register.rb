2cb689731dcab747105ea2c19cc49d4624ee0b8d
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-03 21:46:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,9 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount, :last_transaction_amount, :items
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+    @items = []
+  end
